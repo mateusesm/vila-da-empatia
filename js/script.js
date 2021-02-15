dbf123161c62f6e0bfb71739cdfeb61c4957dc84
Mateus Macedo <74915414+mateusesm@users.noreply.github.com>
2021-02-15 16:06:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@ let show = true;
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = document.querySelector(".menu-toggle")
 
+function clicar () {
+    document.body.style.overflow = show ? "hidden" : "initial"
+    menuSection.classList.toggle("on", show)
+    show = !show;
+}
+
+//Outra maneira de construir menu
 /*menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     show = !show;
 })*/
-function clicar () {
-    menuSection.classList.toggle("on", show)
-    show = !show;
-}
