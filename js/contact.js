192efb9fae479dbb96131a111df150978ce035cb
Mateus Macedo <74915414+mateusesm@users.noreply.github.com>
2021-03-02 21:41:56 -0300

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,34 @@
-var txtbox  = document.querySelectorAll(".txtbox")
-txtbox.color  = 'red'
+const txtbox = document.querySelector(".txtbox")
+const mailbox = document.querySelector(".mailbox")
+const msgbox = document.querySelector(".msgbox")
+
+/*PRIMEIRO INPUT*/
+function addClass () {
+    txtbox.classList.toggle("paint")
+}
+
+function removeClass () {
+    txtbox.classList.remove("paint")
+}
+
+
+/*SEGUNDO INPUT*/
+function addClass2 () {
+    mailbox.classList.toggle("paint")
+}
+
+function removeClass2 () {
+    mailbox.classList.remove("paint")
+}
+
+
+/*TERCEIRO INPUT*/
+function addClass3 () {
+    msgbox.classList.toggle("paint")
+}
+
+function removeClass3 () {
+    msgbox.classList.remove("paint")
+}
+
+
